9aafebb188f2b03a93d49ba01508799d3cab119e
João Dias <ContactJoaoDias@Gmail.com>
2023-11-20 23:55:50 +0000

    Finished the exercise

--- Leet Code 6 - 13. Roman to Integer.py
+++ Leet Code 6 - 13. Roman to Integer.py
@@ -25,7 +25,34 @@
 
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+        
+        # Dictionary to map Roman numerals to their corresponding values
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        prev_value = 0
+
+        # Iterate through the Roman numeral string from right to left
+        for char in reversed(s):
+            value = roman_values[char]
+
+            # If the current value is less than the previous value, subtract it
+            if value < prev_value:
+                result -= value
+            else:
+                # Otherwise, add it to the result
+                result += value
+
+            # Update the previous value for the next iteration
+            prev_value = value
+
+        return result
+    
