dd87036c614ed9c88403f834aa194539e130532d
João Dias <ContactJoaoDias@Gmail.com>
2023-11-24 18:26:53 +0000

    Finished the exercise

--- Leet Code 8 - 14. Longest Common Prefix.py
+++ Leet Code 8 - 14. Longest Common Prefix.py
@@ -1,3 +1,33 @@
 # Write a function to find the longest common prefix string amongst an array of strings.
 
 # If there is no common prefix, return an empty string "".
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        # Sort the list of strings to ensure that the common prefix is at the beginning
+        strs.sort()
+
+        # Take the first and last strings (after sorting)
+        first_str = strs[0]
+        last_str = strs[-1]
+
+        # Find the common prefix between the first and last strings
+        common_prefix = []
+        for i in range(len(first_str)):
+            if i < len(last_str) and first_str[i] == last_str[i]:
+                common_prefix.append(first_str[i])
+            else:
+                break
+
+        # Convert the list of characters back to a string
+        return "".join(common_prefix)
+
+
+solution = Solution()
+input_strings = ["flower", "flow", "flight"]
+result = solution.longestCommonPrefix(input_strings)
+print(result)
